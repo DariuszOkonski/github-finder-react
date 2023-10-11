565a842aa38e20c04a1de0159fed5e4b992db8e6
Dariusz Okoński <darek200180@gmail.com>
2023-10-11 22:31:08 +0200

    came accross rate-limits

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null,
   };
@@ -33,7 +34,6 @@ class App extends Component {
   };
 
   getUser = async (username) => {
-    console.log('getUser: ', username);
     this.setState({ loading: true });
 
     const res = await axios.get(
@@ -42,6 +42,19 @@ class App extends Component {
 
     this.setState({
       user: res.data,
+      loading: false,
+    });
+  };
+
+  getUserRepos = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({
+      repos: res.data,
       loading: false,
     });
   };
@@ -69,7 +82,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, loading, user } = this.state;
+    const { users, loading, user, repos } = this.state;
 
     return (
       <BrowserRouter>
@@ -97,9 +110,11 @@ class App extends Component {
                 path='/user/:login'
                 element={
                   <WrappedUser
-                    user={user}
-                    loading={loading}
                     getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    loading={loading}
+                    repos={repos}
+                    user={user}
                   />
                 }
               />
--- src/components/users/WrappedUser.js
+++ src/components/users/WrappedUser.js
@@ -8,11 +8,16 @@ const WrappedUser = (props) => {
   return (
     <User
       user={props.user}
+      repos={props.repos}
       loading={props.loading}
       getUser={props.getUser}
       login={login}
+      getUserRepos={props.getUserRepos}
     />
   );
 };
 
 export default WrappedUser;
+
+// https://api.github.com/search/users?q=bradtraversy&client_id=24d1361162793dfdd08a&client_secret=c0a5a07d3f54c776a5c1306a2c7a8b736915b9a8
+// https://api.github.com/users/bradtraversy?client_id=24d1361162793dfdd08a&client_secret=c0a5a07d3f54c776a5c1306a2c7a8b736915b9a8
